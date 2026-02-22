9e995ab69bf08ec685b088073f40641f75fe12e1
IyuszTempest <aditwibubawang@gmail.com>
2026-02-22 11:41:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,20 +9,26 @@ const validateKey = (req, res, next) => {
     if (apikey === 'masamba') return next();
     res.status(403).json({ status: false, message: "Apikey salah!" });
 };
-
-// Endpoint untuk Bot WA
-app.get('/api/anime', validateKey, async (req, res) => {
-    const result = await scrapeAnime();
-    res.json({ status: true, creator: "IyuszTempest", result });
+try {
+        switch (feature) {
+            case 'lahelu':
+                const resultLahelu = await handleLahelu();
+                return res.status(200).json(resultLahelu);
+            
+            case 'bluearchive':
+                const resultBA = await handleBlueArchive(query);
+                return res.status(200).json(resultBA);
+            
+            default:
+                res.status(400).json({
+                    status: 'error',
+                    message: "Pilih feature: lahelu atau bluearchive"
+                });
+        }
+    } catch (error) {
+        res.status(500).json({ status: 'error', message: error.message });
+    }
 });
-
-app.get('/api/ai', validateKey, async (req, res) => {
-    const { query } = req.query;
-    if (!query) return res.json({ msg: "Tanya apa?" });
-    const result = await chatAI(query);
-    res.json({ status: true, result });
-});
-
 app.listen(PORT, () => {
     console.log(`Server running di https://iyusztempest.my.id`);
 });
