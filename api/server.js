90c82f42de7f0d9c227b29f4ec477c89d83d6804
IyuszTempest <aditwibubawang@gmail.com>
2026-02-23 12:19:59 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -213,23 +213,18 @@ app.get('/api/tools', checkApikey, async (req, res) => {
     const { feature, query } = req.query; 
     try {
         switch (feature) {
+            case 'presetam': 
+                return res.json(await features.handlePresetAM());
             case 'sub4unlock':
                 if (!query) return res.status(400).json({ msg: "Masukkan link sub4unlock!" });
                 return res.json(await features.handleSub4Unlock(query));
-            
-            case 'presetam': 
-                return res.json(await features.handlePresetAM());
-            
             case 'happymod':
                 if (!query) return res.status(400).json({ msg: "Keyword pencarian wajib diisi!" });
                 return res.json(await features.handleHappymod(query));
-
             default: 
                 return res.status(400).json({ status: false, msg: "Feature Tools tidak ditemukan" });
         }
-    } catch (e) { 
-        res.status(500).json({ status: false, msg: e.message }); 
-    }
+    } catch (e) { res.status(500).json({ status: false, msg: e.message }); }
 });
 
 // Export untuk Vercel
