c880260139d4e84f37deef65e520379baac26330
IyuszTempest <aditwibubawang@gmail.com>
2026-02-24 10:18:57 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -36,7 +36,6 @@ const listFeatures = {
     Downloader: [
         { name: "AIO Downloader", path: "/api/download?feature=aio&url=", desc: "Download media dari berbagai sosial media" },
         { name: "TikTok Downloader", path: "/api/download?feature=tiktok&url=", desc: "Download Video (No WM) atau Slide Foto TikTok" },
-        { name: "YouTube MP3V2", path: "/api/download?feature=ytmp3v2&url=", desc: "Download lagu dari YouTube" },
         { name: "YouTube MP3", path: "/api/download?feature=ytmp3&url=", desc: "Convert YouTube ke Audio" },
         { name: "YouTube MP4", path: "/api/download?feature=ytmp4&url=", desc: "Convert YouTube ke Video" },
         { name: "Play Music", path: "/api/download?feature=play&query=", desc: "Cari & Download Musik" },
@@ -134,10 +133,7 @@ app.get('/api/download', checkApikey, async (req, res) => {
                 return res.json(await features.handleAio(url));
             case 'tiktok': 
                 return res.json(await features.handleTikTok(url));
-            case 'ytmp3v2': 
-                return res.json(await features.handleYtmp3V2(url));
             case 'ytmp3': 
-                if (!url) return res.status(400).json({ status: false, msg: "URL YouTube wajib diisi!" });
                 return res.json(await features.handleYtmp3(url));
             case 'ytmp4': 
                 if (!url) return res.status(400).json({ status: false, msg: "URL YouTube wajib diisi!" }); 
